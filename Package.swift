8b2fb2a28baf40989bf5834e6d5493f6bee1585f
vigneshpillai24 <97335968+vigneshpillai24@users.noreply.github.com>
2025-12-25 20:44:16 +0530

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -15,8 +15,7 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "BugKit",
-            // This URL matches your repo name and the tag 1.0.0
-            url: "https://github.com/mba-developers/BugKit-IOS-Distribution/releases/download/1.0.0/BugKit.xcframework.zip",
+            url: "https://github.com/mba-developers/BugKit-IOS-Distribution/releases/download/0.0.1/BugKit.xcframework.zip",
             checksum: "97d81a522b9e7222c2c92840d9c6d23d568baba2e057ec2646ed3237a3ebd41f"
         )
     ]
